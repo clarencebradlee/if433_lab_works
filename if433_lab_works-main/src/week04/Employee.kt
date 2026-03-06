fca0662b729f5516774b1159c0f4caa792d9331d
Faturachman Alkahfi <faturachman@student.umn.ac.id>
2026-03-06 10:57:30 +0700

    week04:task(2)create developer class

--- if433_lab_works-main/src/week04/Employee.kt
+++ if433_lab_works-main/src/week04/Employee.kt
@@ -2,10 +2,10 @@ package week04
 
 open class Employee(val name: String, val baseSalary: Int) {
     open fun work() {
-        [span_10](start_span)println("$name sedang bekerja.")[span_10](end_span)
+        println("$name sedang bekerja.")
     }
 
     open fun calculateBonus(): Int {
-        [span_11](start_span)return (baseSalary * 0.1).toInt() // Bonus 10%[span_11](end_span)
+        return (baseSalary * 0.1).toInt()
     }
 }
