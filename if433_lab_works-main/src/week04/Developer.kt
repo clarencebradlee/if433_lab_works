d624ad03cefd8be738f3edde5c9e3902a15d4182
Faturachman Alkahfi <faturachman@student.umn.ac.id>
2026-03-06 10:58:35 +0700

    week04:task(2)create developer class

--- if433_lab_works-main/src/week04/Developer.kt
+++ if433_lab_works-main/src/week04/Developer.kt
@@ -2,7 +2,6 @@ package week04
 
 class Developer(name: String, baseSalary: Int, val programmingLanguage: String) : Employee(name, baseSalary) {
     override fun work() {
-        [span_18](start_span)println("$name sedang ngoding menggunakan $programmingLanguage.")[span_18](end_span)
+        println("$name sedang ngoding menggunakan $programmingLanguage.")
     }
-    [span_19](start_span)// calculateBonus tidak di-override (ikut parent)[span_19](end_span)
 }
