79d70f4317f6527d6390ee6fd6c09399b6e759d3
Faturachman Alkahfi <faturachman@student.umn.ac.id>
2026-03-06 10:58:10 +0700

    week04:task(2)create manager class overriding employee

--- if433_lab_works-main/src/week04/Manager.kt
+++ if433_lab_works-main/src/week04/Manager.kt
@@ -2,11 +2,10 @@ package week04
 
 class Manager(name: String, baseSalary: Int) : Employee(name, baseSalary) {
     override fun work() {
-        [span_14](start_span)println("$name sedang memimpin rapat divisi.")[span_14](end_span)
+        println("$name sedang memimpin rapat divisi.")
     }
 
     override fun calculateBonus(): Int {
-        [span_15](start_span)// Bonus standar + 500.000[span_15](end_span)
         return super.calculateBonus() + 500000
     }
 }
